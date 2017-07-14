179a393e8fd652a0197300dd580e33ceca1a2a92
Bora TÜZÜN <boratzn94@gmail.com>
2017-07-14 16:56:31 +0300

    Update capture.py

--- capture.py
+++ capture.py
@@ -10,7 +10,7 @@ while(True):
     gray = cv2.cvtColor(frame, 1)
 # Display the resulting frame
     cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):    #İşletim sisteminiz 64 bit ise 0xFF ile ve'lemeniz gerekir.
         break
 # When everything done, release the capture
     cam.release()
